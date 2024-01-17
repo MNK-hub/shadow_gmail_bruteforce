83b39b0ca8a6908ae3c7a89b4dc5dbfa6b20d818
Shadow-team <142887209+MNK-hub@users.noreply.github.com>
2024-01-17 08:53:35 +0600

    Add files via upload

---
+++ kali_installation.sh
@@ -0,0 +1,5 @@
+apt update && apt update
+apt install ruby -y
+gem install artii
+gem install colorize
+pt install openssl -y
---
+++ termux_installation.sh
@@ -0,0 +1,7 @@
+apt update && apt update -y
+pkg install ruby -y
+pkg install openssl -y
+gem install artii
+pkg install python -y
+pip install colorize
+gem install colorize
